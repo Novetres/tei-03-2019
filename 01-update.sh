747a08b8bc78dd1bd83d21fb6896e76b04a9cc7e
Paloma Kimberly <57427551+Novetres@users.noreply.github.com>
2020-11-17 20:48:13 -0300

    Adicionei automação Ubuntu Server

--- 01-update.sh
+++ 01-update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-clear
 
 echo -e "Atualizando as listas do Apt"   
     apt update
@@ -13,7 +12,7 @@ echo -e "Forçando a atualização do sistema"
 echo -e "Forçando a atualização de segurança"
     apt -y dist-upgrade    
 
-echo -e "Removendo softwares desnecessários"
+echo -e "Removendo software"
     apt -y autoremove
     apt -y autoclean
     apt clean
